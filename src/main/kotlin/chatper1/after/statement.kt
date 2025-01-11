13ee17cf466a2a18869abdd7ef669c6d54504e3f
huiseung <whtjddn75@gmail.com>
2025-01-12 04:00:23 +0900

    [refactor] 예제 1 리팩토링 준비 - 테스트 코드 작성 - before와 똑같은 코드 after에 작성

---
+++ src/main/kotlin/chatper1/after/statement.kt
@@ -0,0 +1,68 @@
+package chatper1.after
+
+
+import Invoice
+import Play
+import chatper1.util.readJson
+import java.text.NumberFormat
+import java.util.*
+import kotlin.math.floor
+import kotlin.math.max
+
+fun statement(invoice: Invoice, plays: Map<String, Play>): String {
+    var totalAmount = 0
+    var volumeCredits = 0
+    var result = "청구 내역 (고객명: ${invoice.customer})\n"
+    val formatter = NumberFormat.getNumberInstance(Locale.US)
+    formatter.minimumFractionDigits = 2 // 소수점 최소 두 자리
+
+    for (perf in invoice.performances) {
+        val play = plays.getOrElse(perf.playID) {
+            Play("unknown", "unknown")
+        }
+        var thisAmount = 0
+
+        when (play.type) {
+            "tragedy" -> {
+                thisAmount = 40_000
+                if (perf.audience > 30) {
+                    thisAmount += 1_000 * (perf.audience - 30)
+                }
+            }
+
+            "comedy" -> {
+                thisAmount = 30_000
+                if (perf.audience > 20) {
+                    thisAmount += 10_000 + 500 * (perf.audience - 20)
+                }
+                thisAmount += 300 * perf.audience
+            }
+
+            else -> throw Error("알 수 없는 장르: ${play.type}")
+        }
+
+        volumeCredits += max(perf.audience - 30, 0)
+        if("comedy" == play.type){
+            volumeCredits += floor(perf.audience.toDouble() / 5).toInt()
+        }
+        result += "  ${play.name}: \$${formatter.format(thisAmount.toDouble()/100)} (${perf.audience})석\n"
+        totalAmount += thisAmount
+    }
+    result += "총액: \$${formatter.format(totalAmount/100)}\n"
+    result += "적립 포인트: ${volumeCredits}점\n"
+    return result
+}
+
+
+fun main() {
+    val playFileName = "plays.json"
+    val invoiceFileName = "invoices.json"
+
+    val plays = readJson<Map<String, Play>>(playFileName)
+    val invoices = readJson<List<Invoice>>(invoiceFileName)
+    val invoice = invoices[0]
+
+    println(statement(invoice, plays))
+}
+
+
---
+++ src/test/kotlin/chapter1/after/StatementTest.kt
@@ -0,0 +1,45 @@
+package chapter1.after
+
+import Invoice
+import Performance
+import Play
+import chatper1.after.statement
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class StatementTest {
+    @Test
+    fun `statement should return correct result for valid input`() {
+        // given
+        val plays = mapOf(
+            "hamlet" to Play("Hamlet", "tragedy"),
+            "as-like" to Play("As You Like It", "comedy"),
+            "othello" to Play("Othello", "tragedy")
+        )
+
+        val invoice = Invoice(
+            customer = "BigCo",
+            performances = listOf(
+                Performance("hamlet", 55),
+                Performance("as-like", 35),
+                Performance("othello", 40)
+            )
+        )
+
+        val expected = """
+            청구 내역 (고객명: BigCo)
+              Hamlet: $650.00 (55)석
+              As You Like It: $580.00 (35)석
+              Othello: $500.00 (40)석
+            총액: $1,730.00
+            적립 포인트: 47점
+            
+        """.trimIndent()
+
+        // when
+        val result = statement(invoice, plays)
+
+        // then
+        assertEquals(expected, result)
+    }
+}
